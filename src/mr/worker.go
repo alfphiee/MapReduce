0ff81b6ac1b73fc21dda631678ebb7239841f5f9
Alfie Binnie <alfiebinnie@gmail.com>
2024-04-10 00:15:34 +0100

    Removed print & Delay from worker

--- src/mr/worker.go
+++ src/mr/worker.go
@@ -36,17 +36,12 @@ func ihash(key string) int {
 //
 func Worker(mapf func(string, string) []KeyValue,
 	reducef func(string, []string) string) {
-
-	// Your worker implementation here.
 	pid := os.Getpid()
 	workerID := strconv.Itoa(pid)
 
-	// uncomment to send the Example RPC to the coordinator.
-	// CallExample()
 	toBreak := false
 	for {
 		task := RequestTask(workerID)
-		fmt.Println(task)
 		switch task.TaskType {
 		case "MapTask":
 			intermediateFiles := applyMap(task, mapf)
@@ -64,8 +59,6 @@ func Worker(mapf func(string, string) []KeyValue,
 		if toBreak {
 			break
 		}
-
-		time.Sleep(5*time.Second)
 
 	}
 
@@ -95,35 +88,6 @@ func NotifyReduceTaskComplete(taskId int) {
 	reply := ReduceTaskDoneReply{}
 
 	call("Coordinator.ReduceTaskCompleted", &args, &reply)
-}
-
-//
-// example function to show how to make an RPC call to the coordinator.
-//
-// the RPC argument and reply types are defined in rpc.go.
-//
-func CallExample() {
-
-	// declare an argument structure.
-	args := ExampleArgs{}
-
-	// fill in the argument(s).
-	args.X = 99
-
-	// declare a reply structure.
-	reply := ExampleReply{}
-
-	// send the RPC request, wait for the reply.
-	// the "Coordinator.Example" tells the
-	// receiving server that we'd like to call
-	// the Example() method of struct Coordinator.
-	ok := call("Coordinator.Example", &args, &reply)
-	if ok {
-		// reply.Y should be 100.
-		fmt.Printf("reply.Y %v\n", reply.Y)
-	} else {
-		fmt.Printf("call failed!\n")
-	}
 }
 
 //
